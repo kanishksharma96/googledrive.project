445e1fdac52f5154da748a97fc551038ce342090
DESKTOP-OECVIU1\Kanishk <kanishks96@gmail.com>
2022-08-22 09:54:41 +0530

    Removed Heroku properties | Kanishk

---
+++ src/main/java/test/com/assignment/gdrive/plugin/drive/service/ActivityMonitorSchedulerTest.java
@@ -0,0 +1,126 @@
+package test.com.assignment.gdrive.plugin.drive.service; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+
+public class ActivityMonitorSchedulerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: init() 
+* 
+*/ 
+
+
+/** 
+* 
+* Method: startPolling() 
+* 
+*/ 
+@Test
+public void testStartPolling() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: stopPolling() 
+* 
+*/ 
+@Test
+public void testStopPolling() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: monitorDriveActivity() 
+* 
+*/ 
+@Test
+public void testMonitorDriveActivity() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ActivityMonitorScheduler.getClass().getMethod("monitorDriveActivity"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: buildQueryDriveAcitivityRequest() 
+* 
+*/ 
+@Test
+public void testBuildQueryDriveAcitivityRequest() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ActivityMonitorScheduler.getClass().getMethod("buildQueryDriveAcitivityRequest"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: buildQueryFilter() 
+* 
+*/ 
+@Test
+public void testBuildQueryFilter() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ActivityMonitorScheduler.getClass().getMethod("buildQueryFilter"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: processDriveActivity() 
+* 
+*/ 
+@Test
+public void testProcessDriveActivity() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = ActivityMonitorScheduler.getClass().getMethod("processDriveActivity"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
---
+++ src/main/java/test/com/assignment/gdrive/plugin/drive/service/DriveActivityServiceTest.java
@@ -0,0 +1,215 @@
+package test.com.assignment.gdrive.plugin.drive.service; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* DriveActivityService Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Aug 22, 2022</pre> 
+* @version 1.0 
+*/ 
+public class DriveActivityServiceTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: queryOnDrive(QueryDriveActivityRequest queryRequest) 
+* 
+*/ 
+@Test
+public void testQueryOnDrive() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: authorize() 
+* 
+*/ 
+@Test
+public void testAuthorize() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDriveActivityService() 
+* 
+*/ 
+@Test
+public void testGetDriveActivityService() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: truncated(List<String> array) 
+* 
+*/ 
+@Test
+public void testTruncated() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("truncated", List<String>.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: truncatedTo(List<String> array, int limit) 
+* 
+*/ 
+@Test
+public void testTruncatedTo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("truncatedTo", List<String>.class, int.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getOneOf(AbstractMap<String, T> obj) 
+* 
+*/ 
+@Test
+public void testGetOneOf() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getOneOf", AbstractMap<String,.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getTimeInfo(DriveActivity activity) 
+* 
+*/ 
+@Test
+public void testGetTimeInfo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getTimeInfo", DriveActivity.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getActionInfo(ActionDetail actionDetail) 
+* 
+*/ 
+@Test
+public void testGetActionInfo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getActionInfo", ActionDetail.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getUserInfo(User user) 
+* 
+*/ 
+@Test
+public void testGetUserInfo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getUserInfo", User.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getActorInfo(Actor actor) 
+* 
+*/ 
+@Test
+public void testGetActorInfo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getActorInfo", Actor.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+/** 
+* 
+* Method: getTargetInfo(Target target) 
+* 
+*/ 
+@Test
+public void testGetTargetInfo() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = DriveActivityService.getClass().getMethod("getTargetInfo", Target.class); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 
